d00c25b2aa568c252ed7e9d051dda01e3cd6aebf
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-07 21:11:55 +0530

    feat: import TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, SESSION_SECRET from env

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const path = require('path')
 const express = require('express')
 const passport = require('passport')
 const { Strategy } = require('passport-twitter')
+const { TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, SESSION_SECRET } = process.env;
