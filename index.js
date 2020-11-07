8a48caeecaa05b2c294688da5385ec47a6f40562
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-07 21:26:59 +0530

    feat: configure route destination

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const passport = require('passport')
 const { Strategy } = require('passport-twitter')
 const { TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, SESSION_SECRET } = process.env;
 const port = process.env.PORT || 3000;
+const app = express();
+const routes = require('./routes');
