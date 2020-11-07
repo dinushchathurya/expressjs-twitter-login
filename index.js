25f2bdac0704e183b590910c1c5972ae1c2298f2
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-07 16:38:01 +0530

    feat: import installes dependencies to index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
 require('dotenv').config();
+const path = require('path')
+const express = require('express')
+const passport = require('passport')
+const { Strategy } = require('passport-twitter')
