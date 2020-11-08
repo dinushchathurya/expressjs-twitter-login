6e85f6cb91e6e21a94934dc18dfa9a81ca145658
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-08 11:22:47 +0530

    feat: define routes for login, logout and login-failure redirect

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,23 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    const { user } = req;
+    res.render('home', { user });
+});
+
+router.get('/login/twitter', passport.authenticate('twitter'));
+
+router.get('/return',
+    passport.authenticate('twitter', { failureRedirect: '/' }),
+    (req, res, next) => {
+        res.redirect('/');
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
